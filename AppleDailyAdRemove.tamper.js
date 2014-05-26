9c85793738ce83595a9627d82984611524037168
river <river@pchomepay.com.tw>
2014-05-26 09:51:32 +0800

    Add Apple Daily Ad Remover

---
+++ AppleDailyAdRemove.tamper.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name       Apple Daily AD Remover
+// @namespace  http://use.i.E.your.homepage/
+// @version    0.1
+// @match      http://*.gamme.com.tw
+// @match      http://*.appledaily.com.tw
+// @copyright  2012+, You
+// ==/UserScript==
+
+$(function(){
+	$(".fb_iframe_widget").hide()
+});
