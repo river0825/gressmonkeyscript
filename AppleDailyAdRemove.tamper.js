57e2b2c0e963b8fee126f15861742c8f4f52bc18
river <river@pchomepay.com.tw>
2014-05-26 10:09:56 +0800

    add description

--- AppleDailyAdRemove.tamper.js
+++ AppleDailyAdRemove.tamper.js
@@ -1,11 +1,22 @@
 // ==UserScript==
-// @name       Apple Daily AD Remover
-// @namespace  http://use.i.E.your.homepage/
-// @version    0.1
-// @match      http://*.gamme.com.tw
-// @match      http://*.appledaily.com.tw
-// @copyright  2012+, You
+// @id             Apple_Daily_AD_Remover
+// @name           Apple Daily AD Remover
+// @version        0.16.13.20140525.1703
+// @namespace      https://github.com/river0825/gressmonkeyscript
+// @updateURL      https://github.com/river0825/gressmonkeyscript/blob/master/AppleDailyAdRemove.tamper.js
+// @downloadURL    https://github.com/river0825/gressmonkeyscript/blob/master/AppleDailyAdRemove.tamper.js
+// @description    remove ad from apple daily
+// @include        http://*.gamme.com.tw/*
+// @include        https://*.gamme.com.tw/*
+// @include        http://*.appledaily.com.tw/*
+// @include        https://*.appledaily.com.tw/*
+// @match          http://*.gamme.com.tw/*
+// @match          https://*.gamme.com.tw/*
+// @match          http://*.appledaily.com.tw/*
+// @match          https://*.appledaily.com.tw/*
+// @grant          none
 // ==/UserScript==
+
 
 $(function(){
 	$(".fb_iframe_widget").hide()
